ab82d6396ccbb74ffa057301cf3141d3a828c2c2
evfredericksen <evfredericksen@gmail.com>
2015-02-15 14:02:36 -0500

    test build script

--- pynhost/pynhost/grammars/_locals.py
+++ pynhost/pynhost/grammars/_locals.py
@@ -1,89 +1,12 @@
 HOMONYMS = {
-    'above': ['about', 'out of'],
-    'begin': ['began'],
-    'below': ['the low', 'a low'],
-    'bill': ['phil'],
-    'block': ['blocks', 'walk'],
-    'boat': ['boat'],
-    'bow': ['bo'],
-    'braces': ['places', 'place', 'brace', 'base', 'race'],
-    'camel': ['kimmel'],
-    'carb': ['car', 'carbon'],
-    'check': ['checks', 'checked'],
-    'climb': ['client', 'climbed'],
-    'copy': ['coffee', 'copies'],
-    'create': ['creates'],
-    'cut': ['collect'],
-    'dict': ['dick', 'deck', 'dock', 'talk', 'decked', 'addict', 'debt'],
-    'dir': ['dear', 'deer'],
-    'disable': ['disabled'],
-    'django': ['jingle', 'jingo', 'jango', 'mango', 'gang of', 'tango', 'jungle'],
-    'enter': ['and there'],
-    'fish': ['dish', 'dished', 'pitch', 'finished', 'fiction', 'page', 'fishing'],
-    'grab': ['lab', 'crab'],
-    'kill': ['killed', 'kills', 'kilt'],
-    'high': ['hi', 'hide'],
-    'lapse': ['laps'],
-    'left': ['last'],
-    'lend': ['land', 'lender'],
-    'line': ['wine', 'lawn'],
-    'list': ['west'],
-    'lit': ["let's", 'wet'],
-    'map': ['matt'],
-    'mint': ['meant', "didn't"],
-    'mod': ['modest'],
-    'nerd': ['learned', 'third', 'noted'],
-    'num': ['numb'],
-    'open': ['opened', 'opens'],
-    'paste': ['based'],
-    'perl': ['pearl', 'rural', 'moral', 'all'],
-    'poke': ['hope', 'poked', 'polk'],
-    'project': ['product'],
-    'recur': ['occur', 'weaker'],
-    'right': ['rights', 'light', 'write'],
-    'root': ['roots', 'route', 'routes'],
-    'select': ['selected'],
-    'short': ['should'],
-    'slap': ['slot'],
-    'start': ['skirt', 'starts', 'starting', 'started', 'serve'],
-    'strip': ['strap'],
-    'stop': ['stopped', 'stops'],
-    'sys': ['cysts'],
-    'variable': ['very important'],
-    'vim': ['them'],
-    'tab': ['have'],
-    'tall': ['paul'],
-    'wave': ['dave', 'ways', 'leave', 'will leave'],
-    'window': ['when the', 'when']
+	'line': ['wine', 'dine'],
+	'perl': ['pearl', 'whirl'],
 }
 
 SLEEP_PATTERNS = [
-    'go to sleep',
-    'snooze',
+	
 ]
 
 WAKE_UP_PATTERNS = [
-    'wake up',
-]
-
-BEGIN_DICTATION_PATTERNS = [
-    'begin dictation',
-    'dictation mode on',
-]
-
-END_DICTATION_PATTERNS = [
-    'end dictation',
-    'and dictation',
-    'dictation mode off',
-]
-
-BEGIN_NUMBER_MODE_PATTERNS = [
-    'begin number mode',
-    'number mode on',
-]
-
-END_NUMBER_MODE_PATTERNS = [
-    'end number mode',
-    'and number mode',
-    'number mode off',
+	
 ]

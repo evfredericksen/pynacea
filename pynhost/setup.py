bb084f7ab7a25a0c851d9dc1a504efadea468bb8
evfredericksen <evfredericksen@gmail.com>
2015-02-16 00:41:22 -0500

    version 0.3.2

--- pynhost/setup.py
+++ pynhost/setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
 	name='pynhost',
-	version='0.3.1',
+	version='0.3.2',
 	description='Linux Voice Recognition System',
 	url='https://github.com/evfredericksen/pynacea',
 	author='Evan Fredericksen',

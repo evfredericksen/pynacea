7d730c967714c99084a35eb1011591f1a3b626ed
evfredericksen <evfredericksen@gmail.com>
2015-03-23 00:55:49 -0400

    create platforms package

---
+++ pynhost/pynhost/platforms/iohandler.py
@@ -0,0 +1,16 @@
+'''
+collection of linux-specific I/O functions
+'''
+
+import subprocess
+import configparser
+import argparse
+import os
+import shutil
+import re
+import sys
+import copy
+
+class IOHandler:
+    def __init__(self):
+        pass

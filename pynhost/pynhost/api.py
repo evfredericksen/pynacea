95844510fbf40f2acd04f81814bf0d00d5c4609a
evfredericksen <evfredericksen@gmail.com>
2015-04-28 01:43:50 -0400

    remove get_homophone from api module

--- pynhost/pynhost/api.py
+++ pynhost/pynhost/api.py
@@ -2,7 +2,6 @@ import subprocess
 import sys
 from pynhost import utilities, keyinput
 from pynhost.platforms import platformhandler
-from pynhost.grammars import _locals
 
 def send_string(string_to_send, delay=0):
     tokenized_keys = keyinput.tokenize_keypresses(string_to_send)
@@ -13,12 +12,3 @@ def mouse_move(x=None, y=None, relative=True):
 
 def mouse_click(button='left', direction='both', number='1'):
     platformhandler.mouse_click(button, direction, number)
-
-def get_homophone(word):
-    '''
-    Replicate <hom> functionality in functions
-    '''
-    for hom in _locals.HOMOPHONES:
-        if word in _locals.HOMOPHONES[hom]:
-            return hom
-    return word
